fe7dfdc000e1165ba50281f8b0cf34d966296306
Uijin Lee <100072451+dml113@users.noreply.github.com>
2025-03-15 13:08:39 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,22 +32,6 @@ module "vpc" {
       "kubernetes.io/role/elb" = 1
     }
 
-    private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-    private_subnet_names = ["my-private-subnet-a" , "my-private-subnet-c"]
-    private_subnet_tags = {
-      "kubernetes.io/role/internal-elb" = 1,
-    }
-
-    # database_subnets = ["10.0.5.0/24", "10.0.6.0/24"]
-    # database_subnet_names = ["my-db-subnet-a", "my-db-subnet-b"]
-
-    # create_database_subnet_group = true
-    # create_database_subnet_route_table = true
-
-    enable_nat_gateway = true
-    single_nat_gateway = false
-    one_nat_gateway_per_az = true
-
     enable_dns_hostnames = true
     enable_dns_support   = true
 }
